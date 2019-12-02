9b23333974d64a1f429b3ff874dfac9c4dd99560
ntbutwal <ntbutwal@gmail.com>
2019-12-02 18:20:58 -0500

    TKT 76782

---
+++ src/main/java/com/kkc/DtoAccount/DtoCustomer.java
@@ -0,0 +1,41 @@
+package com.kkc.DtoAccount;
+
+public class DtoCustomer {
+	private String bankAccount;
+	private String name;
+	private String phoneNumber;
+	private String email;
+
+	public String getBankAccount() {
+		return bankAccount;
+	}
+
+	public void setBankAccount(String bankAccount) {
+		this.bankAccount = bankAccount;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
