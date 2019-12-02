bba601c0bed1cefa1e6c2d46e47c8d102d77ccab
Kishor Kunwar <kishorkc@Kishors-MacBook-Pro.local>
2019-12-02 18:06:05 -0500

    TKT-101 new class added

---
+++ src/main/java/com/kkc/DtoAccount/DtoUsers.java
@@ -0,0 +1,41 @@
+package com.kkc.DtoAccount;
+
+public class DtoUsers {
+	private String name;
+	private String address;
+	private String PhoneNo;
+	private String email;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhoneNo() {
+		return PhoneNo;
+	}
+
+	public void setPhoneNo(String phoneNo) {
+		PhoneNo = phoneNo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
